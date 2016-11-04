14170abe3fa49e04f817269be58f3cf20b5cb62f
Yevhen Kozhoma <straightouttadn@yandex.ru>
2016-11-05 01:52:03 +0200

    Task 2 added

--- hw_1.cpp
+++ hw_1.cpp
@@ -18,8 +18,24 @@ void is_even_bitwise(int x){
 	cout << "bitwise: " << (x & 1) << endl;
 	
 }
-	
 
+//Задание 2.
+//===================================================================
+	
+int sum_of_digits(int x){
+	
+	int sum=0;
+	int hundreds, tens, ones;
+	hundreds = x/100;
+	x = x % 100;
+	tens = x/10;
+	x = x % 10;
+	ones = x;
+	sum = hundreds + tens + ones;
+	
+	return sum;
+}	
+	
 int main(){
 
 	int num;
@@ -27,5 +43,12 @@ int main(){
 	cin >> num;
 	is_even(num); //Задание 1
 	is_even_bitwise(num); // Задание 1
+	cout << "===================================================" << endl;
+//	Задание 2
+	int h_num;
+	cout << "Enter the number: ";
+	cin >> h_num;
+	cout << "Sum of digits: " << sum_of_digits(h_num) << endl;	
+
 	return 0;
 }
