e382733063bdd3485d3043895d371d323c509739
Yevhen Kozhoma <straightouttadn@yandex.ru>
2016-11-05 19:10:11 +0200

    Task 4 added

--- hw_1.cpp
+++ hw_1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <bitset>
-#include <stdio.h>
+#include <cmath>
 using namespace std;
 
 //Задание 1.
@@ -34,21 +34,45 @@ int sum_of_digits(int x){
 	sum = hundreds + tens + ones;
 	
 	return sum;
-}	
+}
+
+// Задание 4.
+//===================================================================
+void cross_points(double x1, double y1, double r1, double x2, \
+double y2, double r2) {
+
+	double dist;
+	//dist = ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	//if ((r1 + r2)*(r1 + r2) >= dist)
+	dist = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+	if (r1 + r2 >= dist && r1 + dist >= r2 && r2 + dist >= r1)
+		cout << "Circles are crossing" << endl;
+	else
+		cout << "Circles are not crossing" << endl;
+	
+}
 	
 int main(){
 
+//	Задание 1
 	int num;
 	cout << "Enter the integer number:";
 	cin >> num;
-	is_even(num); //Задание 1
-	is_even_bitwise(num); // Задание 1
+	is_even(num); 
+	is_even_bitwise(num); 
 	cout << "===================================================" << endl;
 //	Задание 2
 	int h_num;
 	cout << "Enter the number: ";
 	cin >> h_num;
-	cout << "Sum of digits: " << sum_of_digits(h_num) << endl;	
-
+	cout << "Sum of digits: " << sum_of_digits(h_num) << endl;
+	cout << "===================================================" << endl;
+//	Задание 4	
+	double x1, y1, r1, x2, y2, r2;
+	cout << "Circle 1 (x y r): ";
+	cin >> x1 >> y1 >> r1;
+	cout << "Circle 2 (x y r): ";
+	cin >> x2 >> y2 >> r2;
+	cross_points(x1, y1, r1, x2, y2, r2);
 	return 0;
 }
