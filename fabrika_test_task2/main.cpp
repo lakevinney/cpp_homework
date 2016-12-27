e1c1b4cedf9b5870fef0a9158a8aa478bf8ace8a
Yevhen Kozhoma <straightouttadn@yandex.ru>
2016-12-27 22:52:20 +0200

    Task 2 of test completed

--- fabrika_test_task2/main.cpp
+++ fabrika_test_task2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -7,9 +8,12 @@ using namespace std;
 class HTMLElement
 {
 
+private:
+
     unsigned m_width;
     unsigned m_height;
     bool hidden;
+
 public:
 
     HTMLElement(): m_width(0), m_height(0), hidden(false)
@@ -17,21 +21,152 @@ public:
         cout << __PRETTY_FUNCTION__ << endl;
     }
 
-    HTMLElement(unsigned w, unsigned h, bool h): m_width(w), m_height(h), hidden(h)
+    HTMLElement(unsigned wid, unsigned hei, bool hid): m_width(wid), m_height(hei), hidden(hid)
     {
         cout << __PRETTY_FUNCTION__ << endl;
     }
 
+    virtual ~HTMLElement()
+    {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
 
-    unsigned Width() const;
-    unsigned Height() const;
-    bool Hidden() const;
+    unsigned Width() const
+    {
+        return m_width;
+    }
+
+    unsigned Height() const
+    {
+        return m_height;
+    }
+
+    bool Hidden() const
+    {
+        return hidden;
+    }
+
     virtual string Render()=0;
 
 };
 
-int main(int argc, char *argv[])
+class HTMLButtonElement : public HTMLElement
 {
-    cout << "Hello World!" << endl;
+    string m_title;
+public:
+
+    HTMLButtonElement() : HTMLElement(), m_title("None")
+    {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    HTMLButtonElement(unsigned wid, unsigned hei, unsigned hid, const string& title) : HTMLElement(wid, hei, hid), m_title(title)
+    {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    ~HTMLButtonElement()
+    {
+       cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    const string& Title() const
+    {
+        return m_title;
+    }
+
+    string Render() override
+    {
+        return "<button>" + m_title + "</button>:";
+
+    }
+};
+
+class HTMLImageElement: public HTMLElement
+{
+    string m_url;
+
+public:
+    HTMLImageElement() : HTMLElement(), m_url("None")
+    {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    HTMLImageElement(unsigned wid, unsigned hei, unsigned hid, const string& url) : HTMLElement(wid, hei, hid), m_url(url)
+    {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    ~HTMLImageElement()
+    {
+       cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    const string& Url() const
+    {
+        return m_url;
+    }
+
+    string Render() override
+    {
+        return "<img src=" + m_url + " height=" + to_string(Height()) + " width=" + to_string(Width()) +">";
+
+    }
+};
+
+class HTMLTextAreaElement : public HTMLElement
+{
+    string m_content;
+
+public:
+
+    HTMLTextAreaElement() : HTMLElement(), m_content("None")
+    {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    HTMLTextAreaElement(unsigned wid, unsigned hei, unsigned hid, const string& content) : HTMLElement(wid, hei, hid), m_content(content)
+    {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    ~HTMLTextAreaElement()
+    {
+       cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    const string& Content() const
+    {
+        return m_content;
+    }
+
+    string Render() override
+    {
+        return "<textarea>\n" + m_content + "\n</textarea>";
+
+    }
+
+};
+
+
+int main()
+{
+
+    HTMLButtonElement* but = new HTMLButtonElement(10, 20, false, "Register");
+    HTMLImageElement* img = new HTMLImageElement(50, 50, true, "http://www.pictures.com/picture.jpg");
+    HTMLTextAreaElement* txt = new HTMLTextAreaElement(20, 100, false, "Some text");
+
+    cout << but->Title() << endl;
+    cout << img->Url() << endl;
+    cout << txt->Content() << endl;
+
+    const int arrSize = 3;
+    HTMLElement* elems[arrSize] = {but, img, txt};
+
+    for(size_t i = 0; i != arrSize; ++i)
+        assert(cout << elems[i]->Render() << endl);
+
+    for(size_t i = 0; i != arrSize; ++i)
+        delete elems[i];
     return 0;
 }
