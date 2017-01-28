bae7aeb6291f4094653bf4906d50232808141e53
Yevhen Kozhoma <straightouttadn@yandex.ru>
2017-01-28 23:09:25 +0200

    minor fix

--- task_23/main.cpp
+++ task_23/main.cpp
@@ -2,7 +2,7 @@
 #include <map>
 #include <string>
 #include <cassert>
-
+#include <algorithm>
 using namespace std;
 
 class RomanNumber
@@ -10,13 +10,13 @@ class RomanNumber
 
 public:
 
-    RomanNumber() : m_roman(""), m_numeral(0)
+    RomanNumber() : m_roman(""), m_numeral(0), m_synchronized(true)
     {
         cout << __PRETTY_FUNCTION__ << endl;
     }
 
     RomanNumber(const string& roman) : m_roman(roman), \
-        m_numeral(RomanToArabic(roman))
+        m_numeral(ToArabic(roman)), m_synchronized(true)
     {
         cout << __PRETTY_FUNCTION__ << endl;
     }
@@ -33,12 +33,82 @@ public:
 
     const string& GetRoman() const
     {
+        if(!IsSync())
+        {
+            m_roman = ToRoman(m_numeral);
+            m_synchronized = true;
+        }
+
         return m_roman;
     }
 
     const int& GetNumeral() const
     {
         return m_numeral;
+    }
+
+    const bool& IsSync() const
+    {
+        return m_synchronized;
+    }
+
+    static const string ToRoman(int arabic)
+    {
+
+        struct uConvert { int value; char const* numeral; };
+        static uConvert const convMap[] =
+         { 1000, "M",
+            900, "CM",
+            500, "D",
+            400, "CD",
+            100, "C",
+             90, "XC",
+             50, "L",
+             40, "XL",
+             10, "X",
+              9, "IX",
+              5, "V",
+              4, "IV",
+              1, "I",
+              0, NULL };
+
+        string res;
+        for (uConvert const* current = convMap; current->value > 0; ++current)
+
+          while (arabic >= current->value)
+          {
+              res += current->numeral;
+              arabic  -= current->value;
+          }
+
+        return res;
+    }
+
+    static int ToArabic(const string& rom)
+    {
+        static const map<char, int> dConvert = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+
+        int res = 0;
+        int prvs = 0;
+
+        for(auto i = rom.rbegin(); i != rom.rend(); ++i)
+
+           for(auto j = dConvert.begin(); j != dConvert.end(); ++j)
+
+               if(*i == j->first)
+               {
+                  if(j->second >= prvs)
+                  {
+                    res += j->second;
+                    prvs = j->second;
+                  }
+
+                  else
+
+                    res -= j->second;
+               }
+
+        return res;
     }
 
     RomanNumber& operator=(const RomanNumber& rhs)
@@ -71,72 +141,64 @@ public:
 
     RomanNumber& operator+=(const RomanNumber& rhs)
     {
-        int num = m_numeral + rhs.m_numeral;
-        RomanNumber tmp(ArabicToRoman(num));
-        *this = tmp;
+        m_numeral += rhs.m_numeral;
+        m_synchronized = false;
 
         return *this;
     }
 
     RomanNumber& operator+=(const int& rhs)
     {
-        int num = m_numeral + rhs;
-        RomanNumber tmp(ArabicToRoman(num));
-        *this = tmp;
+        m_numeral += rhs;
+        m_synchronized = false;
 
         return *this;
     }
 
     RomanNumber& operator-=(const RomanNumber& rhs)
     {
-        int num = m_numeral - rhs.m_numeral;
-        RomanNumber tmp(ArabicToRoman(num));
-        *this = tmp;
+        m_numeral -= rhs.m_numeral;
+        m_synchronized = false;
 
         return *this;
     }
 
     RomanNumber& operator-=(const int& rhs)
     {
-        int num = m_numeral - rhs;
-        RomanNumber tmp(ArabicToRoman(num));
-        *this = tmp;
+        m_numeral -= rhs;
+        m_synchronized = false;
 
         return *this;
     }
 
     RomanNumber& operator*=(const RomanNumber& rhs)
     {
-        int num = m_numeral * rhs.m_numeral;
-        RomanNumber tmp(ArabicToRoman(num));
-        *this = tmp;
+        m_numeral *= rhs.m_numeral;
+        m_synchronized = false;
 
         return *this;
     }
 
     RomanNumber& operator*=(const int& rhs)
     {
-        int num = m_numeral * rhs;
-        RomanNumber tmp(ArabicToRoman(num));
-        *this = tmp;
+        m_numeral *= rhs;
+        m_synchronized = false;
 
         return *this;
     }
 
     RomanNumber& operator/=(const RomanNumber& rhs)
     {
-        int num = m_numeral / rhs.m_numeral;
-        RomanNumber tmp(ArabicToRoman(num));
-        *this = tmp;
+        m_numeral /= rhs.m_numeral;
+        m_synchronized = false;
 
         return *this;
     }
 
     RomanNumber& operator/=(const int& rhs)
     {
-        int num = m_numeral / rhs;
-        RomanNumber tmp(ArabicToRoman(num));
-        *this = tmp;
+        m_numeral /= rhs;
+        m_synchronized = false;
 
         return *this;
     }
@@ -166,20 +228,12 @@ public:
 
     bool operator==(const RomanNumber& rhs)
     {
-        if(m_numeral == rhs.m_numeral)
-
-            return true;
-
-        return false;
+        return m_numeral == rhs.m_numeral;
     }
 
     bool operator==(const int& rhs)
     {
-        if(m_numeral == rhs)
-
-            return true;
-
-        return false;
+        return m_numeral == rhs;
     }
 
     bool operator!=(const RomanNumber& rhs)
@@ -194,21 +248,12 @@ public:
 
     bool operator<(const RomanNumber& rhs)
     {
-
-        if(m_numeral < rhs.m_numeral)
-
-            return true;
-
-        return false;
+        return m_numeral < rhs.m_numeral;
     }
 
     bool operator<(const int& rhs)
     {
-        if(m_numeral < rhs)
-
-            return true;
-
-        return false;
+        return m_numeral < rhs;
     }
 
     bool operator<=(const RomanNumber& rhs)
@@ -271,78 +316,19 @@ public:
         return old;
     }
 
-    friend ostream& operator<<(ostream& os, const RomanNumber& obj);
-    friend istream& operator>>(istream& is, RomanNumber& c);
+    friend ostream& operator<<(ostream& os, const RomanNumber&);
+    friend istream& operator>>(istream& is, RomanNumber&);
 
-    operator int() const
+    explicit operator int() const
     {
         return m_numeral;
     }
 
 private:
 
-    string m_roman;
+    mutable string m_roman;
     int m_numeral;
-
-    int RomanToArabic(const string& rom)
-        {
-            map<char, int> dConvert = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
-
-            int res = 0;
-            int prvs = 0;
-
-            for(auto i = rom.rbegin(); i != rom.rend(); ++i)
-
-               for(auto j = dConvert.begin(); j != dConvert.end(); ++j)
-
-                   if(*i == j->first)
-                   {
-                      if(j->second >= prvs)
-                      {
-                        res += j->second;
-                        prvs = j->second;
-                      }
-
-                      else
-
-                        res -= j->second;
-                   }
-
-            return res;
-        }
-
-    const string ArabicToRoman(int arabic)
-        {
-
-            struct uConvert { int value; char const* numeral; };
-            static uConvert const convMap[] =
-             { 1000, "M",
-                900, "CM",
-                500, "D",
-                400, "CD",
-                100, "C",
-                 90, "XC",
-                 50, "L",
-                 40, "XL",
-                 10, "X",
-                  9, "IX",
-                  5, "V",
-                  4, "IV",
-                  1, "I",
-                  0, NULL };
-
-            string res;
-            for (uConvert const* current = convMap; current->value > 0; ++current)
-
-              while (arabic >= current->value)
-              {
-                  res += current->numeral;
-                  arabic  -= current->value;
-              }
-
-            return res;
-        }
-
+    mutable bool m_synchronized;
 };
 
 //RomanNumber operator+(const RomanNumber& lhs, const RomanNumber& rhs)
@@ -387,7 +373,8 @@ ostream& operator<<(ostream& os, const RomanNumber& obj)
 istream& operator>>(istream &is, RomanNumber& obj)
 {
     is >> obj.m_roman;
-    obj.m_numeral = obj.RomanToArabic(obj.m_roman);
+    std::transform(obj.m_roman.begin(), obj.m_roman.end(), obj.m_roman.begin(), ::toupper);
+    obj.m_numeral = obj.ToArabic(obj.m_roman);
 
     return is;
 }
@@ -404,10 +391,16 @@ int main()
     assert(r1 >  r2);
     assert(r1 != r2);
 
-    RomanNumber r3;
-    cin >> r3;
-    cout << r3;
-    cout << int(r3) << endl;
+    r1 += r2;
+    assert(!r1.IsSync());
+    cout << r1;
+    assert(r1.IsSync());
+//    RomanNumber r3;
+//    cin >> r3;
+//    cout << r3;
+//    cout << int(r3) << endl;
+
+
     return 0;
 }
 
